aeba097693cf2307800810fee3b6f205747ecbf7
ooeunz <yuns994@gmail.com>
2020-07-19 14:57:57 +0900

    Install babel plugin

---
+++ 01-build-prj/src/simple4.js
@@ -0,0 +1,27 @@
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  render() {
+    return (
+      <div>
+        <LikeButton />
+        <div style={{ marginTop: 20 }}>
+          <span>현재 카운트: </span>
+          <span>{this.state.count}</span>
+          <button
+            onClick={() => this.setState({ count: this.state.count + 1 })}
+          >
+            증가
+          </button>
+          <button
+            onClick={() => this.setState({ count: this.state.count - 1 })}
+          >
+            감소
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
